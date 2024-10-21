e16a37d99b3b82ba26307fc74c413ce2ac1f61db
Eliana Gaul <eliana.gaul@gmail.com>
2024-10-20 20:17:12 -0400

    adding modified index.html and schedule.html

--- join_now.js
+++ join_now.js
@@ -1,5 +1,5 @@
- // Function to handle form submission
- document.getElementById('registrationForm').addEventListener('submit', function(event) {
+// Function to handle form submission
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get the values from the form
@@ -32,58 +32,58 @@
     console.log(item[0]); // we got it!
 
     //if password len less than 5
-    if(password.length < 5){
+    if (password.length < 5) {
         item[0].classList.add("error-li");//turns first list element red 
         item[0].classList.remove("correct-li");
         document.getElementById('message').innerHTML = 'Your password needs to be at least 5 characters long.';
         document.getElementById('message').style.color = 'red'
         return;
     }
-    else{
+    else {
         item[0].classList.remove("error-li");
         item[0].classList.add("correct-li");//turns first list element green
         //document.getElementById('message').innerHTML = '';//clear message
     }
     //if password does not contain a number.
-    if(!hasNumbers){
+    if (!hasNumbers) {
         item[1].classList.add("error-li");//turns second list element red 
         item[1].classList.remove("correct-li");
         document.getElementById('message').innerHTML = 'Your password needs to be at least 1 number.';
         document.getElementById('message').style.color = 'red'
         return;
     }
-    else{
+    else {
         item[1].classList.remove("error-li");
         item[1].classList.add("correct-li");//turns second list element green
         //document.getElementById('message').innerHTML = '';//clear message
     }
     //if password has spaces
-    if(hasSpaces){
+    if (hasSpaces) {
         item[2].classList.add("error-li");//turns third list element red
         item[2].classList.remove("correct-li");
         document.getElementById('message').innerHTML = 'Your password has spaces. Stop that.';
         document.getElementById('message').style.color = 'red'
         return;
     }
-    else{
+    else {
         item[2].classList.remove("error-li");
         item[2].classList.add("correct-li");//turns third list element green
         //document.getElementById('message').innerHTML = '';//clear message
     }
     //if password does not have a special character
-    if(!hasSpecialChar){
+    if (!hasSpecialChar) {
         item[3].classList.add("error-li");//turns fourth list element red
         item[3].classList.remove("correct-li");
         document.getElementById('message').innerHTML = 'Your password needs a spceial character.';
         document.getElementById('message').style.color = 'red'
         return;
     }
-    else{
+    else {
         item[3].classList.remove("error-li");
         item[3].classList.add("correct-li");//turns fourth list element green
         //document.getElementById('message').innerHTML = '';//clear message
     }
-                
+
     // // Check if password is valid
     // if (password.length < 5 || !hasNumbers || hasSpaces || !hasSpecialChar) {
     //     document.getElementById('message').innerHTML = 'Your password needs to be at least 5 characters long, contain a number, no spaces, and at least one special character.';
@@ -98,7 +98,7 @@
         lastName: lastName,
         membershipType: membershipType,
         email: email,
-        password: password 
+        password: password
     };
 
     // Add the new member to the existing members array
